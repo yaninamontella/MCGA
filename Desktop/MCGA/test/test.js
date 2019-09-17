b59caa66a3313333bef3b97505018693f2b59b4d
yaninamontella <yani_montella@hotmail.com>
2019-09-17 16:39:12 -0300

    first commit unit test

---
+++ Desktop/MCGA/test/test.js
@@ -0,0 +1,202 @@
+const assert = require('assert')
+const Quad = require('../quad')
+
+describe('Quad', () => {
+
+  describe('constructor', () => {
+
+    it('Should return [0,0] when created without parameters', () => {
+      const data = new Quad()
+      const result = [data._height, data._width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return [0,0] when created with undefined parameters', () => {
+      const data = new Quad(undefined, undefined)
+      const result = [data._height, data._width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return [0,0] when created with numbers', () => {
+      const data = new Quad(5, 10)
+      const result = [data._height, data._width]
+      const expected = [5, 10]
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return [0,0] when created with strings', () => {
+      const data = new Quad('a', 'b')
+      const result = [data._height, data._width]
+      const expected = ['a', 'b']
+      assert.deepEqual(result, expected)
+    })
+  }) 
+
+  describe('set height', () => {
+
+    it('Should return float when the value is float', () => {
+      const data = new Quad(10.5, 0)
+      const result = data._height
+      const expected =10.5
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return int when the value is int', () => {
+      const data = new Quad(999, 0)
+      const result = data._height
+      const expected =999
+      assert.deepEqual(result, expected)
+    })
+  })
+
+  describe('get height', () => {
+
+    it('Should return float when the value is float', () => {
+      const data = new Quad(10.5, 0)
+      const result = data._height
+      const expected =10.5
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return int when the value is int', () => {
+      const data = new Quad(999, 0)
+      const result = data._height
+      const expected =999
+      assert.deepEqual(result, expected)
+    })
+  })
+
+  describe('set width', () => {
+
+    it('Should return float when the value is float', () => {
+      const data = new Quad(0, 10.5)
+      const result = data._width
+      const expected =10.5
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return int when the value is int', () => {
+      const data = new Quad(0, 999)
+      const result = data._width
+      const expected =999
+      assert.deepEqual(result, expected)
+    })
+  })
+
+  describe('get width', () => {
+
+    it('Should return float when the value is float', () => {
+      const data = new Quad(0, 10.5)
+      const result = data._width
+      const expected =10.5
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return int when the value is int', () => {
+      const data = new Quad(0, 999)
+      const result = data._width
+      const expected =999
+      assert.deepEqual(result, expected)
+    })
+  })
+
+  describe('Area', () => {
+
+    it('Should return 0 when is created with a height is 0 ', () => {
+      const data = new Quad(0, 5)
+      const result = data.area
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return 0 when is created with a width is 0 ', () => {
+      const data = new Quad(5, 0)
+      const result = data.area
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return 0 when is created with a width and height = 0 ', () => {
+      const data = new Quad(0, 0)
+      const result = data.area
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return -1 when is created with a width =-1 and height=1 ', () => {
+      const data = new Quad(-1, 1)
+      const result = data.area
+      const expected = -1
+      assert.equal(result, expected)
+    })
+
+    it('Should return -1 when is created with a width =1 and height=-1 ', () => {
+      const data = new Quad(1,-1)
+      const result = data.area
+      const expected = -1
+      assert.equal(result, expected)
+    })
+
+    it('Should return 1 when is created with a width =-1 and height=-1 ', () => {
+      const data = new Quad(-1,-1)
+      const result = data.area
+      const expected = 1
+      assert.equal(result, expected)
+    })
+  })
+
+  describe('calculatePerimeter()', () => {
+
+    it('Should return 10 when is created with a height = 0 and height=5', () => {
+      const data = new Quad(0, 5)
+      const result = data.calculatePerimeter()
+      const expected = 10
+      assert.equal(result, expected)
+    })
+
+    it('Should return 10 when is created with a height = 5 and height=0', () => {
+      const data = new Quad(5, 0)
+      const result = data.calculatePerimeter()
+      const expected = 10
+      assert.equal(result, expected)
+    })
+
+    it('Should return 0 when is created with a height = 0 and height=0', () => {
+      const data = new Quad(0, 0)
+      const result = data.calculatePerimeter()
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return 0 when is created with a height = -1 and height=1', () => {
+      const data = new Quad(-1, 1)
+      const result = data.calculatePerimeter()
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return 0 when is created with a height = 1 and height=-1', () => {
+      const data = new Quad(1,-1)
+      const result = data.calculatePerimeter()
+      const expected = 0
+      assert.equal(result, expected)
+    })
+
+    it('Should return -8 when is created with a height = -2 and height=-2', () => {
+      const data = new Quad(-2,-2)
+      const result = data.calculatePerimeter()
+      const expected = -8
+      assert.equal(result, expected)
+    })
+
+    it('Should return 10 when is created with a height = 2 and height=3', () => {
+      const data = new Quad(2,3)
+      const result = data.calculatePerimeter()
+      const expected = 10
+      assert.equal(result, expected)
+    })
+
+  })
+})  
